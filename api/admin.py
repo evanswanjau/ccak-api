17527f4a2c792c101ec8f766a59105dfc4a4b280
Evans Wanjau <evanswanjau@gmail.com>
2023-05-30 18:33:02 +0300

    Added search to admin model

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from api.models.post import Post
 from api.models.member import Member
 from api.models.socialpost import SocialPost
+from api.models.search import Search
 
 admin.site.register(Post)
 admin.site.register(Member)
 admin.site.register(SocialPost)
+admin.site.register(Search)

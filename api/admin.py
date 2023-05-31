4b9fff720681a031875b29fae41a760c9f5bf089
Evans Wanjau <evanswanjau@gmail.com>
2023-05-31 16:41:52 +0300

    update: added subscriber module in the admin register

--- api/admin.py
+++ api/admin.py
@@ -3,8 +3,10 @@ from api.models.post import Post
 from api.models.member import Member
 from api.models.socialpost import SocialPost
 from api.models.search import Search
+from api.models.subscriber import Subscriber
 
 admin.site.register(Post)
 admin.site.register(Member)
 admin.site.register(SocialPost)
 admin.site.register(Search)
+admin.site.register(Subscriber)

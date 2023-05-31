ffe21a735be398279d14ef19c4f9ccad3bf08f92
Evans Wanjau <evanswanjau@gmail.com>
2023-05-31 15:35:09 +0300

    update: updated post serializer comment

--- api/serializers/post.py
+++ api/serializers/post.py
@@ -3,6 +3,11 @@ from api.models.post import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """
+        Serializer class for the Post model.
+        Provides serialization/deserialization of Post objects.
+        """
+
     class Meta:
         model = Post
         fields = ['id', 'title', 'excerpt', 'content', 'published', 'category', 'image', 'project_status', 'event_date',

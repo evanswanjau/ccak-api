9180cf28e9bd3e764122b4f9e1cdf33b6a51f89f
Evans Wanjau <evanswanjau@gmail.com>
2023-05-31 15:34:27 +0300

    update: updated post methods into PostView class

--- api/views/posts.py
+++ api/views/posts.py
@@ -1,56 +1,131 @@
-"""
-This file handles all view functions
-"""
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from api.models.post import Post
 from api.serializers.post import PostSerializer
 
 
-@api_view(['GET'])
-def posts(request):
-    data = Post.objects.order_by("created_at").all()
-    serializer = PostSerializer(data, many=True)
-    return JsonResponse(serializer.data, safe=False)
+class PostView(APIView):
+    """
+    API endpoint for managing posts.
 
+    Methods:
+    - GET: Retrieve all posts or a specific post by their post ID.
+    - POST: Create a new post.
+    - PUT: Update an existing post by their post ID.
+    - DELETE: Delete an existing post by their post ID.
+    """
 
-@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
-def post(request, post_id=None):
-    if request.method == 'POST':
+    def get(self, request, post_id=None):
+        """
+        Retrieve all posts or a specific post by their post ID.
+
+        Parameters:
+        - request: The HTTP request object.
+        - post_id: The ID of the post to retrieve (optional).
+
+        Returns:
+        - If post_id is provided, returns the serialized post data.
+        - If post_id is None, returns serialized data for all posts.
+
+        HTTP Methods: GET
+        """
+        if post_id is not None:
+            return self.get_single_post(post_id)
+        else:
+            return self.get_all_posts()
+
+    def get_single_post(self, post_id):
+        """
+        Retrieve details of a post by their post ID.
+
+        Parameters:
+        - post_id: The ID of the post to retrieve.
+
+        Returns:
+        - If the post exists, returns the serialized post data.
+        - If the post does not exist, returns an error response.
+        """
+        try:
+            post = Post.objects.get(pk=post_id)
+            serializer = PostSerializer(post)
+            return Response(serializer.data)
+        except Post.DoesNotExist:
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    def get_all_posts(self):
+        """
+        Retrieve all posts.
+
+        Returns:
+        - Serialized data for all posts.
+        """
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Create a new post.
+
+        Parameters:
+        - request: The HTTP request object.
+
+        Returns:
+        - If the post is created successfully, returns the generated token.
+        - If the post data is invalid, returns an error response.
+
+        HTTP Methods: POST
+        """
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, post_id):
+        """
+        Update an existing post by their post ID.
+
+        Parameters:
+        - request: The HTTP request object.
+        - post_id: The ID of the post to update.
+
+        Returns:
+        - If the post exists and the data is valid, returns the updated post data.
+        - If the post does not exist or the data is invalid, returns an error response.
+
+        HTTP Methods: PUT
+        """
         try:
-            data = Post.objects.get(pk=post_id)
+            post = Post.objects.get(pk=post_id)
         except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        if request.method == 'GET':
-            serializer = PostSerializer(data)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
-        elif request.method == 'PATCH':
-            serializer = PostSerializer(data, data=request.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        elif request.method == 'DELETE':
-            serializer = PostSerializer(data)
-            data.delete()
-            return Response(serializer.data)
+    def delete(self, request, post_id):
+        """
+        Delete an existing post by their post ID.
 
+        Parameters:
+        - request: The HTTP request object.
+        - post_id: The ID of the post to delete.
 
-@api_view(['GET'])
-def search_posts(request, keyword, category, limit):
-    if keyword == "all":
-        data = Post.objects.filter(category=category)[:limit]
-    else:
-        data = Post.objects.filter(title__contains=keyword, category=category)
+        Returns:
+        - If the post exists, deletes the post and returns a success response.
+        - If the post does not exist, returns an error response.
 
-    serializer = PostSerializer(data, many=True)
-    return JsonResponse(serializer.data, safe=False)
+        HTTP Methods: DELETE
+        """
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Post.DoesNotExist:
+            return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

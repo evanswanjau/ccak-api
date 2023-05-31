0bbaa93274b6b29fc240158f2c497d36b581eac3
Evans Wanjau <evanswanjau@gmail.com>
2023-05-31 15:11:31 +0300

    update: added new methods to validate member registration

--- api/serializers/member.py
+++ api/serializers/member.py
@@ -1,11 +1,84 @@
+import re
+
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from api.models.member import Member
 
 
 class MemberSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the Member model.
+    Provides validation and serialization/deserialization of Member objects.
+    """
+
     class Meta:
         model = Member
-        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'company', 'designation', 'password', 'bio',
-                  'technology', 'company_email', 'company_phone', 'location', 'website_link', 'logo',
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'company', 'designation', 'password',
+                  'bio', 'technology', 'company_email', 'company_phone', 'location', 'website_link', 'logo',
                   'registration_status', 'subscription_status', 'subscription_category', 'subscription_expiry',
-                  'status', 'step', 'created_by', 'created_at', 'last_updated']
+                  'status', 'step', 'agree_to_terms', 'created_by', 'created_at', 'last_updated']
+
+    @staticmethod
+    def validate_email(email):
+        """
+        Validate the email field.
+
+        Args:
+            email (str): Email address.
+
+        Returns:
+            str: Validated email address.
+
+        Raises:
+            serializers.ValidationError: If the email is not valid or if it already exists.
+        """
+        # Perform email validation here
+        if not re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email):
+            raise serializers.ValidationError("Invalid email address.")
+
+        # Check if email already exists
+        if Member.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Email already exists.')
+        return email
+
+    @staticmethod
+    def validate_phone_number(phone_number):
+        """
+        Validate the phone_number field.
+
+        Args:
+            phone_number (str): Phone number.
+
+        Returns:
+            str: Validated phone number.
+
+        Raises:
+            serializers.ValidationError: If the phone number is not valid or if it already exists.
+        """
+        # Perform phone number validation here
+        if not re.match(r'^\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$', phone_number):
+            raise serializers.ValidationError("Invalid phone number.")
+
+        # Check if phone number already exists
+        if Member.objects.filter(phone_number=phone_number).exists():
+            raise serializers.ValidationError('Phone number already exists.')
+        return phone_number
+
+    def update(self, instance, validated_data):
+        """
+        Update an existing member instance.
+
+        Hashes the password using bcrypt if it is updated.
+
+        Args:
+            instance (Member): The existing member instance.
+            validated_data (dict): Validated data for updating the member.
+
+        Returns:
+            Member: The updated member instance.
+        """
+        # Hash the password using bcrypt if it is updated
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            validated_data['password'] = make_password(password)
+        return super().update(instance, validated_data)

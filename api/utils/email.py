07d1af92ca34d993bff6ca9e66ebaafc79ac920d
Evans Wanjau <evanswanjau@gmail.com>
2023-06-02 00:50:33 +0300

    new: added new default email template

--- api/utils/email.py
+++ api/utils/email.py
@@ -1,25 +1,32 @@
 from django.conf import settings
 from django.core.mail import send_mail
 from email.utils import formataddr
+from django.template.loader import render_to_string
 
 
-def send_email(recipient, subject, message):
+def send_email(recipient, recipient_name, subject, message):
     """
     Sends an email to the specified recipient.
 
     Parameters:
-    - recipient: The email address of the recipient.
-    - subject: The subject of the email.
-    - message: The content of the email.
+    - recipient (str): The email address of the recipient.
+    - recipient_name (str): The name of the recipient.
+    - subject (str): The subject of the email.
+    - message (str): The content of the email.
 
     Returns:
-    - True if the email was sent successfully, False otherwise.
+    - dict: The response from the send_mail function.
+
+    Raises:
+    - Exception: If there is an error while sending the email.
     """
-    try:
-        from_email = formataddr((settings.EMAIL_HOST_NAME, settings.EMAIL_HOST_USER))
-        send_mail(subject, message, from_email, [recipient])
-        return True
-    except Exception as e:
-        # Handle any exceptions that occurred during email sending
-        print(f"Error sending email: {e}")
-        return False
+    context = {
+        'recipient_name': recipient_name,
+        'message': message
+    }
+
+    email_content = render_to_string('emails/default_email.html', context)
+
+    from_email = formataddr((settings.EMAIL_HOST_NAME, settings.EMAIL_HOST_USER))
+
+    return send_mail(subject, None, from_email, [recipient], html_message=email_content)

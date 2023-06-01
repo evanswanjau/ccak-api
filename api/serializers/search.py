f3bb47575a33dd71598e84d1854092162a682d3b
Evans Wanjau <evanswanjau@gmail.com>
2023-06-01 22:36:01 +0300

    update: added technology on search serializers

--- api/serializers/search.py
+++ api/serializers/search.py
@@ -5,5 +5,5 @@ from api.models.search import Search
 class SearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Search
-        fields = ['id', 'keyword', 'table', 'category', 'project_status', 'page', 'limit', 'ip_address', 'created_by',
-                  'created_at']
+        fields = ['id', 'keyword', 'table', 'category', 'technology', 'project_status', 'page', 'limit', 'ip_address',
+                  'created_by', 'created_at']

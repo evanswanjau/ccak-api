aa51b319914f7797caadc55c1013e493f3ffdf5c
Evans Wanjau <evanswanjau@gmail.com>
2023-07-18 16:58:34 +0300

    Updated search to allow invoice and payment searches

--- api/views/search.py
+++ api/views/search.py
@@ -3,8 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from api.models.post import Post
+from api.models.invoice import Invoice
+from api.models.payment import Payment
 from api.models.member import Member
 from api.serializers.post import PostSerializer
+from api.serializers.invoice import InvoiceSerializer
+from api.serializers.payment import PaymentSerializer
 from api.serializers.member import MemberSerializer
 from api.serializers.search import SearchSerializer
 
@@ -118,8 +122,48 @@ def search_members(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['POST'])
+def search_invoices(request):
+    """
+    Search and retrieve a paginated list of invoices based on the provided search criteria.
+
+    Args:
+        request (Request): The HTTP POST request containing the search parameters.
+
+    Returns:
+        Response: A Response object containing the serialized data of the matching invoices.
+
+    Raises:
+        - KeyError: If the 'page' or 'limit' keys are not present in the request data.
+        - Exception: If there is an error while processing the search or serialization.
+    """
+    query = get_invoices_query(request.data)
+
+    offset = get_offset(request.data['page'], request.data['limit'])
+    data = Invoice.objects.filter(**query).order_by("-created_at")[offset["start"]:offset["end"]]
+
+    for invoice in data:
+        invoice = payment_details(invoice)
+
+    serializer = InvoiceSerializer(data, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def search_payments(request):
+    query = get_payments_query(request.data)
+
+    offset = get_offset(request.data['page'], request.data['limit'])
+    data = Payment.objects.filter(**query).order_by("-created_at")[offset["start"]:offset["end"]]
+
+    serializer = PaymentSerializer(data, many=True)
+
+    return Response(serializer.data)
+
+
 def get_posts_query(data):
-    query = {};
+    query = {}
 
     if data['keyword']:
         query["title__contains"] = data['keyword']
@@ -141,6 +185,36 @@ def get_posts_query(data):
     return query
 
 
+def get_invoices_query(data):
+    query = {}
+
+    if data['invoice_number']:
+        query["invoice_number__contains"] = data['invoice_number']
+
+    if data['type']:
+        query["description__contains"] = data['type']
+
+    if data['status']:
+        query["status"] = data['status']
+
+    return query
+
+
+def get_payments_query(data):
+    query = {}
+
+    if data['transaction_id']:
+        query["transaction_id__contains"] = data['transaction_id']
+
+    if data['method']:
+        query["method"] = data['method']
+
+    if data['invoice_number']:
+        query["invoice_number__contains"] = data['invoice_number']
+
+    return query
+
+
 def get_offset(page, limit):
     """
     Calculate the pagination range.
@@ -157,3 +231,31 @@ def get_offset(page, limit):
     start = start + 1 if start != 0 else start
 
     return {"start": start, "end": end}
+
+
+def payment_details(invoice):
+    """
+    Calculates the payment details for an invoice.
+
+    Args:
+        invoice (Invoice): The invoice object for which to calculate the payment details.
+
+    Returns:
+        Invoice: The updated invoice object with total amount, paid amount, and balance calculated.
+    """
+    total_amount = 0
+    paid_amount = 0
+
+    for item in invoice.items:
+        total_amount += item['quantity'] * item['unit_price']
+
+    invoice.total_amount = total_amount
+
+    payments = Payment.objects.filter(invoice_number=invoice.invoice_number)
+    for payment in payments:
+        paid_amount += payment.amount
+
+    invoice.paid_amount = paid_amount
+    invoice.balance = total_amount - paid_amount
+
+    return invoice

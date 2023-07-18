6c56df068d6ec76f64f11c7080df2c79190aa222
Evans Wanjau <evanswanjau@gmail.com>
2023-07-18 04:16:01 +0300

    Updated serializers amount models to read_only

--- api/serializers/invoice.py
+++ api/serializers/invoice.py
@@ -7,9 +7,9 @@ class InvoiceSerializer(serializers.ModelSerializer):
     Serializer for the Invoice model.
     Serializes the Invoice model fields for API interactions.
     """
-    total_amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    paid_amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    balance = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    total_amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00, read_only=True)
+    paid_amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00, read_only=True)
+    balance = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00, read_only=True)
 
     class Meta:
         model = Invoice

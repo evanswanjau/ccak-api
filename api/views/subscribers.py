8295c767164892f0c4becd07d5ed0dbea26b7201
evanswanjau <evanswanjau@gmail.com>
2023-08-21 02:06:32 +0300

    fix: refactored subscribers views

--- api/views/subscribers.py
+++ api/views/subscribers.py
@@ -6,7 +6,7 @@ from api.serializers.subscriber import SubscriberSerializer
 from api.utils.email import send_email
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_subscribers(request):
     """
     Get all subscribers.
@@ -15,20 +15,20 @@ def get_subscribers(request):
     Parameters: None
     Returns: A list of all subscribers on the mailing list.
     """
-    if getattr(request.user, "role", None) not in ["super-admin", "admin", "content-admin"]:
+    if getattr(request.user, "role", None) not in [
+        "super-admin",
+        "admin",
+        "content-admin",
+    ]:
         return Response({"message": "Administrator is not authorized"}, status=403)
 
     subscribers = Subscriber.objects.all()
-
-    # Modify the email field for partial obscuring
-    for subscriber in subscribers:
-        subscriber.email = obscure_email(subscriber.email)
 
     serializer = SubscriberSerializer(subscribers, many=True)
     return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def create_subscriber(request):
     """
     Create a new subscriber.
@@ -43,13 +43,13 @@ def create_subscriber(request):
         serializer.save()
 
         # Send welcome email to subscriber
-        send_welcome_email(request.data.get('email'))
+        send_welcome_email(request.data.get("email"))
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def delete_subscriber(request, subscriber_id):
     """
     Remove a subscriber.
@@ -59,7 +59,11 @@ def delete_subscriber(request, subscriber_id):
         - subscriber_id (int): The ID of the subscriber to be removed.
     Returns: No content if the subscriber is successfully deleted, or a 404 error if the subscriber does not exist.
     """
-    if getattr(request.user, "role", None) not in ["super-admin", "admin", "content-admin"]:
+    if getattr(request.user, "role", None) not in [
+        "super-admin",
+        "admin",
+        "content-admin",
+    ]:
         return Response({"message": "Administrator is not authorized"}, status=403)
 
     try:
@@ -84,16 +88,3 @@ def send_welcome_email(email):
     context = {"recipient_name": "subscriber"}
 
     send_email(email, subject, context, "subscriber_email.html")
-
-
-def obscure_email(email):
-    username, domain = email.split('@')
-    partial_username = username[:3]
-
-    obscured_username = partial_username + 'x' * (len(username) - 3)
-    partial_domain = domain[-2:]
-
-    obscured_domain = 'x' * (len(domain) - 2) + partial_domain
-    partial = obscured_username + '@' + obscured_domain
-
-    return partial

50d84bbf82147741bb91f3c1052f6d27251f2b25
evanswanjau <evanswanjau@gmail.com>
2023-08-25 19:16:09 +0300

    Added password reset link functionality

--- api/views/auth.py
+++ api/views/auth.py
@@ -1,4 +1,4 @@
-import bcrypt
+import os
 
 from django.contrib.auth.hashers import check_password, make_password
 from rest_framework.decorators import api_view
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from api.models.member import Member
 from api.models.administrator import Administrator
+from api.utils.email import send_email
 
 
 @api_view(["POST"])
@@ -105,3 +106,62 @@ def administrator_login(request):
             {"error": "Email and password are required."},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+@api_view(["POST"])
+def reset_link(request):
+    """
+    Reset Link API Endpoint
+
+    This endpoint allows the user to request a password reset link.
+
+    Parameters:
+        - request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Response (Response): A response containing the result of the request.
+    """
+    email = request.data.get("email")
+    user_type = request.data.get("user_type")
+
+    user = None
+    if user_type == "administrator":
+        user = Administrator.objects.filter(email=email).first()
+
+    if user_type == "member":
+        user = Member.objects.filter(email=email).first()
+
+    # Generate tokens
+    refresh = RefreshToken.for_user(user)
+    refresh["user_type"] = user_type  # Add custom claim to the payload
+    refresh.access_token.payload[
+        "user_type"
+    ] = "admin"  # Also add to the access token's payload
+
+    if user and user.email:
+        send_reset_link_email(user, refresh.access_token)
+
+    return Response(
+        {"success": "Your email has been sent successfully"},
+        status=status.HTTP_200_OK,
+    )
+
+
+def send_reset_link_email(user, token):
+    """
+    Sends a password reset link email to the user.
+
+    Args:
+        token: A secure token
+        user (Member): The user object.
+
+    Returns:
+        dict: The response from the send_email function.
+    """
+    subject = "Reset Your CCAK Password"
+    context = {
+        "recipient_name": user.first_name,
+        "token": token,
+        "url": os.getenv("FRONTEND_URL"),
+    }
+    return send_email(user.email, subject, context, "reset_link_email.html")

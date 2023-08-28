d1ba202480d7680aebd25e739fe235dca5d7889d
evanswanjau <evanswanjau@gmail.com>
2023-08-28 20:34:58 +0300

    Added invoice_number to payments field

--- api/models/payment.py
+++ api/models/payment.py
@@ -7,9 +7,10 @@ class Payment(models.Model):
     A schema for a payment query.
     This schema defines the properties of a payment.
     """
+
     transaction_id = models.CharField(max_length=150, blank=True)
     method = models.CharField(max_length=150)
-    invoice_number = models.CharField(max_length=150)
+    invoice_number = models.CharField(max_length=150, blank=True)
     timestamp = models.CharField(max_length=150)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid_by = models.JSONField(default=dict)

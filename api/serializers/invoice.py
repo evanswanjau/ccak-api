e7c1c0a6e269f318f87823ee7c05c040d4f1dbe9
evanswanjau <evanswanjau@gmail.com>
2023-08-31 20:09:13 +0300

    Updated invoice serializers

--- api/serializers/invoice.py
+++ api/serializers/invoice.py
@@ -7,11 +7,32 @@ class InvoiceSerializer(serializers.ModelSerializer):
     Serializer for the Invoice model.
     Serializes the Invoice model fields for API interactions.
     """
-    total_amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00, read_only=True)
-    paid_amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00, read_only=True)
-    balance = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00, read_only=True)
+
+    total_amount = serializers.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00, read_only=True
+    )
+    paid_amount = serializers.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00, read_only=True
+    )
+    balance = serializers.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00, read_only=True
+    )
 
     class Meta:
         model = Invoice
-        fields = ['id', 'invoice_number', 'description', 'items', 'status', 'member_id', 'customer', 'total_amount',
-                  'paid_amount', 'balance', 'created_by', 'created_at', 'last_updated']
+        fields = [
+            "id",
+            "invoice_number",
+            "description",
+            "items",
+            "status",
+            "member_id",
+            "donation_id",
+            "customer",
+            "total_amount",
+            "paid_amount",
+            "balance",
+            "created_by",
+            "created_at",
+            "last_updated",
+        ]
